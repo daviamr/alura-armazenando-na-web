cfd37bce67fdb8b4482b70eab4118e0d35b1480e
Davi (Estudo) <daviamorim_sz@live.com>
2023-08-30 00:09:59 -0300

    comments

--- script/main.js
+++ script/main.js
@@ -1,10 +1,13 @@
 const form = document.getElementById("novoItem");
 const list = document.getElementById("lista");
-const listaDeItens = JSON.parse(localStorage.getItem("items")) || []; //JSON.parse converte o string em array novamente, após isso, localStorage verifique se há itens dentro de array, caso não tenha, crie um array vazio
+
+//JSON.parse converte o string em array novamente, após isso, localStorage verifique se há itens dentro de array, caso não tenha, crie um array vazio
+const listaDeItens = JSON.parse(localStorage.getItem("items")) || [];
 
 listaDeItens.forEach((elemento) => {
   criaElemento(elemento);
 });
+console.log(listaDeItens)
 
 //Evento de submit do formulário
 form.addEventListener("submit", (evento) => {
@@ -12,18 +15,18 @@ form.addEventListener("submit", (evento) => {
   const nome = evento.target.elements["nome"];
   const quantidade = evento.target.elements["quantidade"];
 
-  const existe = listaDeItens.find((elemento) => elemento.nome === nome.value);
+  const existe = listaDeItens.find((elemento) => elemento.nome === nome.value); //Verificando se o elemento já existe dentro da array
 
-  const itemAtual = {
+  const itemAtual = { //Objeto esqueleto do elemento criado
     nome: nome.value,
     quantidade: quantidade.value,
   };
 
-  if (existe) {
+  if (existe) { //Caso o elementro já exista na array faça;
     itemAtual.id = existe.id;
     atualizaElemento(itemAtual);
     listaDeItens[existe.id] = itemAtual;
-  } else {
+  } else { //Caso não exista, faça;
     itemAtual.id = listaDeItens.length;
     criaElemento(itemAtual);
     listaDeItens.push(itemAtual);
@@ -31,7 +34,8 @@ form.addEventListener("submit", (evento) => {
 
   localStorage.setItem("items", JSON.stringify(listaDeItens)); //JSON.stringify convertendo o objetos em string para salvar no localStorage
 
-  nome.value = "";
+  //Restaurando os campos vazioas após o submit
+  nome.value = ""; 
   quantidade.value = "";
 });
 
